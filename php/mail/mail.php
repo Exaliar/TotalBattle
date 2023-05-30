8f8533539db8f1ce3253cadabd5bc26c3a44b5f4
Exaliar <57668254+Exaliar@users.noreply.github.com>
2023-05-30 20:34:15 +0200

    czystki

--- php/mail/mail.php
+++ php/mail/mail.php
@@ -1,13 +1,13 @@
 <?php
    $naglowki ="";
    // Naglowki mozna sformatowac tez w ten sposob.
-   $naglowki ="Reply-to: grzegorz.raczynski@wp.pl <grzegorz.raczynski@wp.pl>".PHP_EOL;
+   $naglowki ="Reply-to: grzegorz@wp.pl <grzegorz@wp.pl>".PHP_EOL;
    $naglowki .="From: tb@wp.pl <tb@wp.pl>".PHP_EOL;
    $naglowki .="MIME-Version: 1.0".PHP_EOL;
    $naglowki .="Content-type: text/html; charset=UTF-8".PHP_EOL; 
 
 function Email ($mail){
-   if(mail('grzegorz.raczynski@wp.pl', 'Witaj test mail', $mail))
+   if(mail('grzegorz@wp.pl', 'Witaj test mail', $mail))
    {
       // echo 'Wiadomość została wysłana';
    }
